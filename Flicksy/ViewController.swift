c5c52a47af8a8911dcd49dbbec3eedac149637ed
Solomakha Dmitry <solomakhadmitry@gmail.com>
2025-04-05 18:03:40 +0300

    Initial Commit

---
+++ Flicksy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flicksy
+//
+//  Created by Дмитрий Соломаха on 05.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
